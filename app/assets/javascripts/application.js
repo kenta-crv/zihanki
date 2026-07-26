936949f5d57f87757403405194de3bb1729cb65e
kenta-crv <okuyamakenta@gmail.com>
2026-07-26 20:21:00 +0900

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,8 +27,8 @@ $(function() {
 
 // jsを再読み込みさせたいページで使う（/companies/pay）
 $(document).on("turbolinks:load", function () {
-  let path_name = location.pathname;
-  let path_search = location.search;
+  var path_name = location.pathname;
+  var path_search = location.search;
   if (path_search == '?reload=true') {
     window.location.href = path_name
   }
