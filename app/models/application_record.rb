826f4b839a4bba4c835afde9ab8e9c402d563018
kenta-crv <okuyamakenta@gmail.com>
2026-07-31 05:28:37 +0900

    fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # Ransack 4 以降は明示的な allowlist が必須
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
 end
